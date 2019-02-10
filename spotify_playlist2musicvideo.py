7ab89d1c84045df56718581bb948892c5caf6ea7
weetgeen <hans-pot@hotmail.com>
2019-02-10 23:32:58 +0100

    download single playlist by ID

--- spotify_playlist2musicvideo.py
+++ spotify_playlist2musicvideo.py
@@ -20,12 +20,56 @@ basefilepath = ""
 scope    = 'user-library-read'
 
 
-def show_tracks(results, playlistname):
+
+
+
+def show_tracks(results, playlistname,basefilepath):
     for i, item in enumerate(results['items']):
         track = item['track']
         print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
 
         youtubeDownload.find_video(track['artists'][0]['name'] + ' ' + track['name'], playlistname, basefilepath)      
+
+def download_all_playlist():    
+	if token:
+		sp = spotipy.Spotify(auth=token)
+		playlists = sp.user_playlists(username)
+		for playlist in playlists['items']:
+			if playlist['owner']['id'] == username:
+				print()
+				print(playlist['name'])
+
+				#Create new folder of not existing
+				youtubeDownload.create_playlist_folder(playlist['name'], basefilepath)
+            
+				print('  total tracks', playlist['tracks']['total'])
+				results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
+				tracks = results['tracks']
+				show_tracks(tracks, playlist['name'],basefilepath)
+				while tracks['next']:
+					tracks = sp.next(tracks)
+					show_tracks(tracks, playlist['name'],basefilepath)
+                
+	else:
+		print("Can't get token for", username)
+
+		
+def download_single_playlist(playlist_id, playlistname):
+    sp = spotipy.Spotify(auth=token)
+    results = sp.user_playlist(username, playlist_id, fields="tracks,next")
+    tracks = results['tracks']
+
+    #Create new folder of not existing
+    youtubeDownload.create_playlist_folder(playlistname, basefilepath)
+    
+    show_tracks(tracks, playlistname, basefilepath)
+    while tracks['next']:
+	    tracks = sp.next(tracks)
+	    show_tracks(tracks, playlistname, basefilepath)
+
+
+
+
 
 try:
     token = util.prompt_for_user_token(username)
@@ -33,25 +77,15 @@ except (AttributeError, JSONDecodeError):
     os.remove(f".cache-{username}")
     token = util.prompt_for_user_token(username)
 
-    
-if token:
-    sp = spotipy.Spotify(auth=token)
-    playlists = sp.user_playlists(username)
-    for playlist in playlists['items']:
-        if playlist['owner']['id'] == username:
-            print()
-            print(playlist['name'])
 
-            #Create new folder of not existing
-            youtubeDownload.create_playlist_folder(playlist['name'], basefilepath)
-            
-            print('  total tracks', playlist['tracks']['total'])
-            results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
-            tracks = results['tracks']
-            show_tracks(tracks, playlist['name'])
-            while tracks['next']:
-                tracks = sp.next(tracks)
-                show_tracks(tracks, playlist['name'])
-                
+print('Provide playlist ID or type ALL to download all your playlists')
+playlist_id = input()  #TODO let users input spotify URI
+
+if(playlist_id == 'ALL'):
+	print('Downloading all playlists')
+	download_all_playlist()
 else:
-    print("Can't get token for", username)
+	#TODO verify user input
+	print('Playlist name')
+	playlistname = input() #TODO get playlist name from spotify
+	download_single_playlist(playlist_id, playlistname)	
